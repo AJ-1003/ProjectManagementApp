57ae456f3ec9c3bc1863f03d6be05c166d6368af
AJ-1003 <ia.jordaan1003@gmail.com>
2025-01-29 23:21:48 +0200

    minor changes

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -7,8 +7,7 @@ Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
-      userPoolClientId:
-        process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
     },
   },
 });
